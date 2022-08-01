a4b6c33a741a46fc3b53fb555755de196b6506d1
ricardomartinso <ricardomartinso.dev@gmail.com>
2022-08-01 19:27:54 -0300

    finished

--- Shortify_postgres_create.sql
+++ Shortify_postgres_create.sql
@@ -1,62 +1,38 @@
-CREATE TABLE "public.customers" (
-	"id" serial NOT NULL,
+CREATE TABLE "customers" (
+	"id" serial PRIMARY KEY NOT NULL,
 	"name" varchar(70) NOT NULL,
 	"password" TEXT NOT NULL,
-	"email" TEXT NOT NULL UNIQUE,
-	"address" TEXT NOT NULL,
-	CONSTRAINT "customers_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
+	"email" TEXT NOT NULL UNIQUE
 );
 
+CREATE TYPE "size" AS ENUM ('PP', 'P', 'M', 'G', 'GG', 'XGG');
 
-
-CREATE TABLE "public.products" (
-	"id" serial NOT NULL,
+CREATE TABLE "products" (
+	"id" serial PRIMARY KEY NOT NULL,
 	"name" varchar(40) NOT NULL,
 	"price" DECIMAL NOT NULL,
-	"size" varchar(1) NOT NULL,
+	type "size" NOT NULL,
 	"imageUrl" TEXT NOT NULL,
 	"secImageUrl" TEXT,
 	"categoryId" integer NOT NULL,
-	CONSTRAINT "products_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
 );
 
-
-
-CREATE TABLE "public.productsCategory" (
-	"id" serial NOT NULL,
+CREATE TABLE "productsCategory" (
+	"id" SERIAL PRIMARY KEY NOT NULL,
 	"name" varchar(40) NOT NULL UNIQUE,
-	CONSTRAINT "productsCategory_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
 );
 
 
-
-CREATE TABLE "public.buyedProducts" (
+CREATE TABLE "buyedProducts" (
 	"userId" integer NOT NULL,
 	"productId" integer NOT NULL,
-	"productQtd" integer NOT NULL DEFAULT '1',
-	"buyedDate" TIMESTAMP NOT NULL DEFAULT 'NOW()',
+	"productQtd" integer NOT NULL DEFAULT 1,
+	"buyedDate" TIMESTAMP NOT NULL DEFAULT NOW(),
 	"customerAddress" TEXT NOT NULL,
 	"orderState" varchar(9) NOT NULL
-) WITH (
-  OIDS=FALSE
 );
 
-
-
-
 ALTER TABLE "products" ADD CONSTRAINT "products_fk0" FOREIGN KEY ("categoryId") REFERENCES "productsCategory"("id");
-
-
 ALTER TABLE "buyedProducts" ADD CONSTRAINT "buyedProducts_fk0" FOREIGN KEY ("userId") REFERENCES "customers"("id");
 ALTER TABLE "buyedProducts" ADD CONSTRAINT "buyedProducts_fk1" FOREIGN KEY ("productId") REFERENCES "products"("id");
-
-
-
-
 
